0b127cfb616ef065ba467da925828f0ad11055a5
Igor Ferreira <igor-freitas10@hotmail.com>
2022-01-19 16:58:26 -0300

    Cálculo de expressões simples entre parênteses

---
+++ operacoes.py
@@ -0,0 +1,33 @@
+def somar(*elementos):
+    return sum(elementos)
+
+
+def subtrair(*elementos):
+    result = None
+    for i, e in enumerate(elementos):
+        if i == 0:
+            result = e
+            continue
+        result -= e
+    return result
+
+
+def multiplicar(*elementos):
+    result = 1
+    for e in elementos:
+        result *= e
+    return result
+
+
+def dividir(*elementos):
+    result = None
+    try:
+        for i, e in enumerate(elementos):
+            if i == 0:
+                result = e
+                continue
+            result /= e
+    except ZeroDivisionError:
+        print('Erro: Não é possível efetuar uma divisão por zero!')
+    else:
+        return result
--- view.py
+++ view.py
@@ -1,23 +1,43 @@
-def somar(a, b):
-    return a + b
+import operacoes as opmat
 
 
-def subtrair(a, b):
-    return a - b
+def calc_exp_simples(exp):
+    exp = exp.replace('(', '').replace(')', '')
+    if '/' in exp:
+        exp = exp.replace(' / ', ' ')
+        operandos = [float(o) for o in exp.split(' ')]
+        result = opmat.dividir(*operandos)
+    elif '*' in exp:
+        exp = exp.replace(' * ', ' ')
+        operandos = [float(o) for o in exp.split(' ')]
+        result = opmat.multiplicar(*operandos)
+    elif '+' in exp:
+        exp = exp.replace(' + ', ' ')
+        operandos = [float(o) for o in exp.split(' ')]
+        result = opmat.dividir(*operandos)
+    else:
+        exp = exp.replace(' - ', ' ')
+        operandos = [float(o) for o in exp.split(' ')]
+        result = opmat.dividir(*operandos)
+
+    return result
 
 
-def multiplicar(a, b):
-    return a * b
+expressao = '2 + 4 + 3 * 3 - (3 * (30 / 10))'
+total_parenteses = expressao.count('(')
 
+if total_parenteses > 0:
+    abre_parenteses = []
+    fecha_parenteses = []
+    for i, c in enumerate(expressao):
+        if c == '(':
+            abre_parenteses.append(i)
+        if c == ')':
+            fecha_parenteses.append(i)
 
-def dividir(a, b):
-    return a / b
+    for i in range(total_parenteses - 1, -1, -1):
+        sub_exp = expressao[abre_parenteses[i]:(fecha_parenteses[::-1][i] + 1)]
+        result_sub_exp = calc_exp_simples(sub_exp)
+        expressao = expressao.replace(sub_exp, str(result_sub_exp))
 
-
-v1 = float(input('Valor 1: ').strip())
-v2 = float(input('Valor 2: ').strip())
-
-print(somar(v1, v2))
-print(subtrair(v1, v2))
-print(multiplicar(v1, v2))
-print(dividir(v1, v2))
+    print(expressao)
