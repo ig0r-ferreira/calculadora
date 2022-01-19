ba2aeec15542494bd232481968c026cdf769a5b3
Igor Ferreira <igor-freitas10@hotmail.com>
2022-01-19 12:12:57 -0300

    Criado o arquivo view.py
    Arquivo inicial que contém as 4 operações básicas da matemática.

---
+++ view.py
@@ -0,0 +1,23 @@
+def somar(a, b):
+    return a + b
+
+
+def subtrair(a, b):
+    return a - b
+
+
+def multiplicar(a, b):
+    return a * b
+
+
+def dividir(a, b):
+    return a / b
+
+
+v1 = float(input('Valor 1: ').strip())
+v2 = float(input('Valor 2: ').strip())
+
+print(somar(v1, v2))
+print(subtrair(v1, v2))
+print(multiplicar(v1, v2))
+print(dividir(v1, v2))
