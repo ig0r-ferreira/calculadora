6ee2aa9a2e6e413e98621354c87c1dc0ff54d946
Igor Ferreira <igor-freitas10@hotmail.com>
2022-01-22 13:10:10 -0300

    Reorganização do main.py

--- main.py
+++ main.py
@@ -1,85 +1,93 @@
 import tkinter.font
-
-from expnum import calc_exp
 from tkinter import *
 from functools import partial
 
 
 def digitar(e):
-    visor['text'] = visor['text'] + str(e)
+    tela_exibicao['text'] = tela_exibicao['text'] + str(e)
 
 
 def apagar():
-    visor['text'] = visor['text'][:-1]
+    tela_exibicao['text'] = tela_exibicao['text'][:-1]
 
 
 def limpar_visor():
-    visor['text'] = ''
+    tela_exibicao['text'] = ''
 
 
-def calcular_resultado():
-    expressao_numerica = visor['text'].replace(',', '.')
+def calcular():
+    from expnum import calc_exp
+
+    expressao_numerica = tela_exibicao['text'].replace(',', '.')
     resultado = calc_exp(expressao_numerica)
     if resultado is not None:
-        visor['text'] = str(resultado).replace('.', ',')
+        tela_exibicao['text'] = str(resultado).replace('.', ',')
 
 
-def criar_botao(local, texto, linha, coluna, acao):
-    botao = Button(local, width=5, height=2, text=texto, command=acao)
+def criar_botao(local, config):
 
-    botao.grid(row=linha, column=coluna, padx=6, pady=8)
+    botao = Button(local,
+                   width=config.get('width'),
+                   height=config.get('height'),
+                   text=config.get('text'),
+                   command=config.get('command')
+                   )
 
-    config_font = tkinter.font.Font(family='Lucida Console', size=15)
-    botao.config(font=config_font)
+    botao.grid(row=config.get('row'), column=config.get('column'), padx=6, pady=8)
+
+    if config.get('font'):
+        font_family = config.get('font').get('family')
+        font_size = config.get('font').get('size')
+        font_weight = config.get('font').get('weight')
+
+        botao.config(font=tkinter.font.Font(
+            family='Lucida Console' if not font_family else font_family,
+            size=15 if not font_size else font_size,
+            weight='normal' if not font_weight else font_weight
+        ))
 
     return botao
 
 
-def gerar_visor(local):
-    visor = Label(master=local, width=23, height=3, font=18, bg='white', fg='black')
-    visor.pack(pady=40)
-
-    config_font = tkinter.font.Font(family='Lucida Console', size=16, weight='bold')
-    visor.config(font=config_font)
-
-    return visor
-
-
-def gerar_painel_botoes(local):
-    botoes_frame = Frame(master=local, width=100, height=100)
-    botoes_frame.pack(padx=25)
-
-    return botoes_frame
-
-
-def gerar_botoes_numericos(local):
-    num = 1
-    config_font = tkinter.font.Font(family='Lucida Console', size=15, weight='bold')
-    for i in range(1, 4):
-        for j in range(0, 3):
-            botao = criar_botao(local, texto=f'{num}', linha=i, coluna=j, acao=partial(digitar, num))
-            botao.config(font=config_font)
-            num = num + 1
-
-    botao_zero = criar_botao(local, texto='0', linha=4, coluna=1, acao=partial(digitar, '0'))
-    botao_zero.config(font=config_font)
-
-
-def gerar_botoes_operacoes(local):
-    operacoes_mat = ['+', '-', '*', '/']
-
-    for i, operador in enumerate(operacoes_mat):
-        criar_botao(local, texto=operador, linha=i + 1, coluna=3, acao=partial(digitar, f' {operador} '))
-
-    criar_botao(local, texto='C', linha=0, coluna=2, acao=limpar_visor)
-    criar_botao(local, texto='<=', linha=0, coluna=3, acao=apagar)
-    criar_botao(local, texto='=', linha=5, coluna=3, acao=calcular_resultado)
-
-
-def gerar_botoes_adicionais(local):
-    criar_botao(local, texto='(', linha=0, coluna=0, acao=partial(digitar, '('))
-    criar_botao(local, texto=')', linha=0, coluna=1, acao=partial(digitar, ')'))
-    criar_botao(local, texto=',', linha=4, coluna=2, acao=partial(digitar, ','))
+def obter_config_botoes():
+    return [
+        [
+            {'identificador': '(', 'acao': partial(digitar, '('), 'font': {}},
+            {'identificador': ')', 'acao': partial(digitar, ')'), 'font': {}},
+            {'identificador': 'C', 'acao': limpar_visor, 'font': {}},
+            {'identificador': '<=', 'acao': apagar, 'font': {}},
+        ],
+        [
+            {'identificador': '1', 'acao': partial(digitar, '1'), 'font': {'weight': 'bold'}},
+            {'identificador': '2', 'acao': partial(digitar, '2'), 'font': {'weight': 'bold'}},
+            {'identificador': '3', 'acao': partial(digitar, '3'), 'font': {'weight': 'bold'}},
+            {'identificador': '+', 'acao': partial(digitar, '+'), 'font': {}},
+        ],
+        [
+            {'identificador': '4', 'acao': partial(digitar, '4'), 'font': {'weight': 'bold'}},
+            {'identificador': '5', 'acao': partial(digitar, '5'), 'font': {'weight': 'bold'}},
+            {'identificador': '6', 'acao': partial(digitar, '6'), 'font': {'weight': 'bold'}},
+            {'identificador': '-', 'acao': partial(digitar, '-'), 'font': {}},
+        ],
+        [
+            {'identificador': '7', 'acao': partial(digitar, '7'), 'font': {'weight': 'bold'}},
+            {'identificador': '8', 'acao': partial(digitar, '8'), 'font': {'weight': 'bold'}},
+            {'identificador': '9', 'acao': partial(digitar, '9'), 'font': {'weight': 'bold'}},
+            {'identificador': '*', 'acao': partial(digitar, '*'), 'font': {}},
+        ],
+        [
+            None,
+            {'identificador': '0', 'acao': partial(digitar, '0'), 'font': {}},
+            {'identificador': ',', 'acao': partial(digitar, ','), 'font': {}},
+            {'identificador': '/', 'acao': partial(digitar, '/'), 'font': {}},
+        ],
+        [
+            None,
+            None,
+            None,
+            {'identificador': '=', 'acao': calcular, 'font': {}},
+        ]
+    ]
 
 
 janela = Tk()
@@ -87,11 +95,37 @@ janela.title('Calculadora')
 janela.resizable(height=False, width=False)
 janela.minsize(400, 610)
 
-visor = gerar_visor(janela)
+tela_exibicao = Label(master=janela, width=23, height=3, font=18, bg='white', fg='black')
+tela_exibicao.pack(pady=40)
+tela_exibicao.config(font=tkinter.font.Font(
+    family='Lucida Console',
+    size=16,
+    weight='bold'
+))
 
-painel_botoes = gerar_painel_botoes(janela)
-gerar_botoes_numericos(painel_botoes)
-gerar_botoes_operacoes(painel_botoes)
-gerar_botoes_adicionais(painel_botoes)
+painel_botoes = Frame(master=janela, width=100, height=100)
+painel_botoes.pack(padx=25)
+
+config_botoes = obter_config_botoes()
+
+for i, fileira in enumerate(config_botoes):
+    for j, botao in enumerate(fileira):
+
+        if botao is None:
+            continue
+
+        criar_botao(painel_botoes, {
+            'width': 5,
+            'height': 2,
+            'text': botao.get('identificador'),
+            'command': botao.get('acao'),
+            'row': i,
+            'column': j,
+            'font': {
+                'family': botao.get('font').get('family'),
+                'size': botao.get('font').get('size'),
+                'weight': botao.get('font').get('weight')
+            }
+        })
 
 janela.mainloop()
